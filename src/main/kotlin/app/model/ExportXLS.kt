bc374a40da7ab8503432d4533fa86554d6c6ac3b
Oliver Kusche <okworx@users.noreply.github.com>
2020-11-07 09:14:45 +0100

    add debug code

--- src/main/kotlin/app/model/ExportXLS.kt
+++ src/main/kotlin/app/model/ExportXLS.kt
@@ -5,8 +5,11 @@ import org.openlca.core.database.IDatabase
 import org.openlca.core.database.ProcessDao
 import org.openlca.io.xls.process.output.ExcelExport
 import java.io.File
+import org.slf4j.LoggerFactory
 
 class ExportXLS : Export {
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     override val format = Format.XLS
 
@@ -14,6 +17,7 @@ class ExportXLS : Export {
 
     override fun doIt(db: IDatabase): File {
         val dir = cache.tempDir()
+  		log.debug("exporting as XLS using temp dir {}", dir)
         val exp = ExcelExport(dir, db, ProcessDao(db).descriptors)
         exp.run()
         return cache.zipFilesAndClean(dir)
